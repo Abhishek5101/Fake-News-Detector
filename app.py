19e3d92518555707e8898f82b8cc73d43c4cb5f4
noirvin <vin.sp97@googlemail.com>
2020-02-02 23:47:07 -0800

    integrated api

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "<h1>Hello Mrs.B</h1>"
+
+    params = {
+
+        "q": "trump",
+
+        "apikey": '69c0ff694a324f2d88e70206ce4b1718',
+
+        "pageSize": 50,
+
+        "country": 'us'
+        }
+    r = requests.get('https://newsapi.org/v2/top-headlines?', params=params)
+    content = r.json()
 
 
+    return content['articles'][0]
 
 
 if __name__ == "__main__":
